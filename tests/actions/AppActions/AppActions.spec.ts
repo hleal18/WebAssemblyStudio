5364bcb93a238368632fca9c95f70455a40cc600
Humberto Leal <humberto.adolfo@hotmail.com>
2018-04-01 22:29:09 -0500

    Test for closeTabs action

--- tests/actions/AppActions/AppActions.spec.ts
+++ tests/actions/AppActions/AppActions.spec.ts
@@ -5,7 +5,9 @@ import * as React from "react";
 
 import appStore from "../../../src/stores/AppStore";
 import dispatcher from "../../../src/dispatcher";
-import {AppActionType, logLn} from "../../../src/actions/AppActions";
+import {AppActionType, logLn, closeTabs, openFile, splitGroup} from "../../../src/actions/AppActions";
+import {File, FileType} from "../../../src/model";
+import {ViewType} from "../../../src/components/editor/View";
 
 describe("AppActions component", () => {
   describe("logLn action", () => {
@@ -30,4 +32,38 @@ describe("AppActions component", () => {
       expect(notified).toBeTruthy();
     });
   });
+
+  describe("closeTabs action", () => {
+    let exampleFiles : File[] = [
+      new File("test1.c", FileType.C),
+      new File("test2.cpp", FileType.Cpp),
+      new File("test3.js", FileType.JavaScript)
+    ];
+    beforeEach(() => {
+      dispatcher.dispatch({type: AppActionType.INIT_STORE});
+    });
+    it("should delete a view related to a file", () => {
+      openFile(exampleFiles[0]);
+      expect(appStore.getActiveTabGroup().views.length).toBe(1);
+      closeTabs(exampleFiles[0]);
+      expect(appStore.getActiveTabGroup().views.length).toBe(0);
+    });
+    it("should delete two views from two groups related to a file", () => {
+      openFile(exampleFiles[0]);
+      splitGroup();
+      expect(appStore.getActiveTabGroup().views.length).toBe(1);
+      expect(appStore.getTabGroups().length).toBe(2);
+      closeTabs(exampleFiles[0]);
+      expect(appStore.getActiveTabGroup().views.length).toBe(0);
+      expect(appStore.getTabGroups().length).toBe(1);
+    });
+    it("should delete one view and place the correct activeTabGroup", () => {
+      openFile(exampleFiles[0]);
+      openFile(exampleFiles[1]);
+      splitGroup();
+      expect(appStore.getActiveTabGroup().views.length).toBe(1);
+      expect(appStore.getTabGroups()[0].views).toBe(2);
+      expect(appStore.getTabGroups().length).toBe(2);
+    })
+  })
 });
